c7766ff338f2731a865445e8ebd9e9e64110cebb
Yuriy Bondar <bondar.europe@gmail.com>
2020-02-19 19:46:07 +0200

    adds home page

---
+++ src/js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  /* menu */
+
+  $('.navbar-toggler').click(function() {
+    $(this).toggleClass('open');
+  });
+
+  $(function() {
+    $(window).scroll(function() {
+      var winTop = $(window).scrollTop();
+      if (winTop >= 60) {
+        $('body').addClass('sticky-header');
+      } else {
+        $('body').removeClass('sticky-header');
+      }
+    });
+  });
+
+  /* carousel */
+
+  $('.home-carousel').owlCarousel({
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 1000,
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+});
+
+new WOW().init();
